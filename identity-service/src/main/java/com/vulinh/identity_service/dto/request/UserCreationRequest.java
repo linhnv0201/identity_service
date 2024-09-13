564c446f88c0be4a0c0de71ff745b0fb93a80cbd
Vu Linh Dev <linh.nv0201@gmail.com>
2024-09-13 15:47:16 +0700

    add lombok

--- identity-service/src/main/java/com/vulinh/identity_service/dto/request/UserCreationRequest.java
+++ identity-service/src/main/java/com/vulinh/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,13 @@
 package com.vulinh.identity_service.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class UserCreationRequest {
     @Size(min = 3, message = "Username must be at least 3 character.")
     private String username;
@@ -12,44 +16,4 @@ public class UserCreationRequest {
     private String firstName;
     private String lastName;
     private LocalDate dob;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }
--- identity-service/src/main/java/com/vulinh/identity_service/dto/request/UserUpdateRequest.java
+++ identity-service/src/main/java/com/vulinh/identity_service/dto/request/UserUpdateRequest.java
@@ -1,42 +1,14 @@
 package com.vulinh.identity_service.dto.request;
 
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+@Getter
+@Setter
 public class UserUpdateRequest {
     private String password;
     private String firstName;
     private String lastName;
     private LocalDate dob;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }
